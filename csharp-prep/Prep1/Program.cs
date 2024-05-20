8ff9d5d71f591a03dabf87771cc7fab775a6fcc8
Brunwyn <tristenbetts799@gmail.com>
2024-05-20 14:21:18 -0600

    fin prep one

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your favorite color? ");
+        string _color = Console.ReadLine();
+        Console.WriteLine($"Your favorite collor is {_color}\n\n");
+
+        Console.Write("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"\nYour name is {lastname}, {firstname} {lastname}.");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
+        // example of print to console
         Console.WriteLine("Hello Sandbox World!");
+
+        // Get a string, Strings (a sequence of characters, including letters, numbers, or symbols)
+        Console.Write("What is your favorite color? ");
+        string color = Console.ReadLine();
+        // example of print a variable
+        Console.Write($"Your favorite color is {color}.");
+
+        // Get an int, Integers (whole numbers, positive and negative)
+
+
+        // Get a float, Floating point numbers (numbers that use decimals)
+
+
+        //  Get a double, Double precision floating point numbers (just like a float, except it takes up twice as much memory, so it can hold larger numbers or numbers with more decimal places
+
+
+        // Get a bool, Boolean variables (true or false)
+
+
+        // example of simple if statement        
+        Console.Write("X is 10");
+        Console.Write("X is 5");
+        int X = 10;
+        int Y = 5;
+        
+        if (X>Y)
+        {
+            Console.WriteLine("X is greater // example of if satement");
+        }
+
+        // example of if else statement
+
+
+        // example of if else elsee
     }
 }
